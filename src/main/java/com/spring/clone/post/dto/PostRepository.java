47683732445b7c7d9712e73b38aeba06b9f2220a
rockintuna <rockintuna@naver.com>
2021-10-18 14:12:05 +0900

    add PostRepository.java

---
+++ src/main/java/com/spring/clone/post/dto/PostRepository.java
@@ -0,0 +1,10 @@
+package com.spring.clone.post.dto;
+
+import com.spring.clone.post.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+    Page<Post> findAllByOrderByCreatedAtDesc(Pageable pageable);
+}
