d9867fbfd0b9ea288c361a61a1ae91ee5cc3c988
rockintuna <rockintuna@naver.com>
2021-10-18 13:11:36 +0900

    add Post.java entity class

---
+++ src/main/java/com/spring/clone/post/Post.java
@@ -0,0 +1,28 @@
+package com.spring.clone.post;
+
+import com.spring.clone.global.entity.Timestamped;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+public class Post extends Timestamped {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+    private String imageUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "USER_ID", nullable = false)
+    private User user;
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL)
+    private Comment comment;
+
+}
