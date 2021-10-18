e04d8c3987ab6497f2e9b2e207b66b27cbb02883
rockintuna <rockintuna@naver.com>
2021-10-18 23:23:08 +0900

    ADD GlobalController AuthenticationServiceException 예외처리기 추가

--- src/main/java/com/spring/clone/exception/GlobalController.java
+++ src/main/java/com/spring/clone/exception/GlobalController.java
@@ -3,6 +3,7 @@ package com.spring.clone.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -16,7 +17,15 @@ public class GlobalController {
     public Map<String, Object> accessDeniedExceptionHandler(AccessDeniedException exception) {
         Map<String, Object> result = new HashMap<>();
         result.put("responseMessage", exception.getMessage());
-        result.put("statusCode", HttpStatus.FORBIDDEN);
+        result.put("statusCode", HttpStatus.FORBIDDEN.value());
+        return result;
+    }
+
+    @ExceptionHandler
+    public Map<String, Object> authenticationServiceException(AuthenticationServiceException exception) {
+        Map<String, Object> result = new HashMap<>();
+        result.put("responseMessage", exception.getMessage());
+        result.put("statusCode", HttpStatus.UNAUTHORIZED.value());
         return result;
     }
 }
