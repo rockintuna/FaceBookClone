ae4c7fe09d75930351c4ab4bd914830190d62a46
rockintuna <rockintuna@naver.com>
2021-10-18 15:06:56 +0900

    ADD Comment.java

---
+++ src/main/java/com/spring/clone/comment/Comment.java
@@ -0,0 +1,30 @@
+package com.spring.clone.comment;
+
+import com.spring.clone.global.entity.Timestamped;
+import com.spring.clone.post.Post;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Comment extends Timestamped {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "POST_ID", nullable = false)
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "USER_ID", nullable = false)
+    private User user;
+}
