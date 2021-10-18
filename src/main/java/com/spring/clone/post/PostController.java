4530342303b5232bbbe87c751eb11982098678a8
rockintuna <rockintuna@naver.com>
2021-10-18 14:07:53 +0900

    edit PostController.java CRUD API

--- src/main/java/com/spring/clone/post/PostController.java
+++ src/main/java/com/spring/clone/post/PostController.java
@@ -1,9 +1,10 @@
 package com.spring.clone.post;
 
+import com.spring.clone.post.dto.PostRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
@@ -15,7 +16,7 @@ public class PostController {
 
     private final PostService postService;
 
-    @GetMapping
+    @GetMapping("/post")
     public Map<String, Object> getPostsOrderByCreatedAtDesc() {
         Map<String, Object> result = new HashMap<>();
         List<Post> posts = postService.getPostsOrderByCreatedAtDesc();
@@ -23,4 +24,40 @@ public class PostController {
         result.put("posts", posts);
         return result;
     }
+
+    @PostMapping("/post")
+    public Map<String, Object> addPost(
+            @AuthenticationPrincipal UserDetails userDetails,
+            @RequestBody PostRequestDto requestDto) {
+        Map<String, Object> result = new HashMap<>();
+        postService.addPost(requestDto);
+
+        result.put("data", null);
+        return result;
+    }
+
+    @PutMapping("/post/{postId}")
+    public Map<String, Object> editPost(
+            @AuthenticationPrincipal UserDetails userDetails,
+            @PathVariable("postId") Long postId,
+            @RequestBody PostRequestDto requestDto) {
+        Map<String, Object> result = new HashMap<>();
+        postService.editPost(postId, requestDto);
+
+        result.put("data", null);
+        return result;
+    }
+
+    @DeleteMapping
+    public Map<String, Object> deletePost(
+            @AuthenticationPrincipal UserDetails userDetails,
+            @PathVariable("postId") Long postId
+    ) {
+        Map<String, Object> result = new HashMap<>();
+        postService.deletePost(postId);
+
+        result.put("data", null);
+        return result;
+    }
+
 }
