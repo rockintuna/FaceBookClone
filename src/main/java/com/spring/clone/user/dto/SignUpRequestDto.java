0a3530391f0d2c8e4843a3ec988d61aa8d4d2dc7
rockintuna <rockintuna@naver.com>
2021-10-19 17:40:40 +0900

    FIX SignUpRequestDto 패스워드 체크 제거

--- src/main/java/com/spring/clone/user/dto/SignUpRequestDto.java
+++ src/main/java/com/spring/clone/user/dto/SignUpRequestDto.java
@@ -16,7 +16,6 @@ import java.time.LocalDate;
 public class SignUpRequestDto {
     private String userId;
     private String pwd;
-    private String pwdCheck;
     private String firstName;
     private String lastName;
     private LocalDate birth;
