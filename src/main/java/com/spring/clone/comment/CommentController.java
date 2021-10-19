5eb801dd2b34893c21ed3bd499b78fba8070d0dc
rockintuna <rockintuna@naver.com>
2021-10-19 09:39:31 +0900

    FIX CommentController 로그인 예외 처리

--- src/main/java/com/spring/clone/comment/CommentController.java
+++ src/main/java/com/spring/clone/comment/CommentController.java
@@ -4,6 +4,7 @@ import com.spring.clone.comment.dto.CommentRequestDto;
 import com.spring.clone.sercurity.UserDetailsImpl;
 import com.spring.clone.user.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,10 +22,11 @@ public class CommentController {
             @AuthenticationPrincipal UserDetailsImpl userDetails,
             @RequestBody CommentRequestDto requestDto) {
         Map<String, Object> result = new HashMap<>();
-        User user = userDetails.getUser();
+        User user = getUserByUserdetailsIfExist(userDetails);
         commentService.addComment(requestDto, user);
 
-        result.put("data", null);
+        result.put("statusCode", 200);
+        result.put("responseMessage", "댓글 생성 성공");
         return result;
     }
 
@@ -33,11 +35,12 @@ public class CommentController {
             @AuthenticationPrincipal UserDetailsImpl userDetails,
             @PathVariable("commentId") Long commentId,
             @RequestBody CommentRequestDto requestDto) {
-        User user = userDetails.getUser();
+        User user = getUserByUserdetailsIfExist(userDetails);
         Map<String, Object> result = new HashMap<>();
         commentService.editComment(commentId, requestDto, user);
 
-        result.put("data", null);
+        result.put("statusCode", 200);
+        result.put("responseMessage", "댓글 수정 성공");
         return result;
     }
 
@@ -46,11 +49,22 @@ public class CommentController {
             @AuthenticationPrincipal UserDetailsImpl userDetails,
             @PathVariable("commentId") Long commentId
     ) {
-        User user = userDetails.getUser();
+        User user = getUserByUserdetailsIfExist(userDetails);
         Map<String, Object> result = new HashMap<>();
         commentService.deleteComment(commentId, user);
 
-        result.put("data", null);
+        result.put("statusCode", 200);
+        result.put("responseMessage", "댓글 삭제 성공");
         return result;
+    }
+
+    private User getUserByUserdetailsIfExist(UserDetailsImpl userDetails) {
+        User user;
+        if ( userDetails != null ) {
+            user = userDetails.getUser();
+        } else {
+            throw new AuthenticationServiceException("로그인이 필요합니다.");
+        }
+        return user;
     }
 }
