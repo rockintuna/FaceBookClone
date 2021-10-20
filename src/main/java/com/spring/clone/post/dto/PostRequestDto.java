f80b6fb84f3ac6d1dbaa627e0eeaa81ba26f1874
rockintuna <rockintuna@naver.com>
2021-10-20 11:28:19 +0900

    FIX PostRequestDto all 생성자 추가

--- src/main/java/com/spring/clone/post/dto/PostRequestDto.java
+++ src/main/java/com/spring/clone/post/dto/PostRequestDto.java
@@ -1,9 +1,11 @@
 package com.spring.clone.post.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
+@AllArgsConstructor
 @NoArgsConstructor
 public class PostRequestDto {
     private String content;
