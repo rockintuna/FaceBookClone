4d3e0f44ad91707c6206762ad35425bd67a31dc1
junseok93 <86007685+junseok93@users.noreply.github.com>
2021-10-21 18:47:34 +0900

    fix webconfig 주석추가제거

--- src/main/java/com/spring/clone/exception/BadArgumentException.java
+++ src/main/java/com/spring/clone/exception/BadArgumentException.java
@@ -2,6 +2,7 @@ package com.spring.clone.exception;
 
 public class BadArgumentException extends RuntimeException {
     public BadArgumentException(String message) {
+
         super(message);
     }
 }
