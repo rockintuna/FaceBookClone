d792c5bf07d63c02a7526964af8a41f064703d80
rockintuna <rockintuna@naver.com>
2021-10-21 19:23:27 +0900

    FIX Timestamped setter 추가

--- src/main/java/com/spring/clone/config/Timestamped.java
+++ src/main/java/com/spring/clone/config/Timestamped.java
@@ -1,6 +1,7 @@
 package com.spring.clone.config;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -10,6 +11,7 @@ import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
 @Getter     // 일괄적으로 get 함수 자동 생성, ****** 까먹지 않아야한다.
+@Setter
 @MappedSuperclass   // 멤버 변수가 컬럼으로 되도록
 @EntityListeners(AuditingEntityListener.class)  // 다른 값이 변경되었을 때, 멤버 변수가 자동으로 변경되도록
 public abstract class Timestamped {
